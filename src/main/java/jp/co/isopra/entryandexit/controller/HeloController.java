1bbd0caa9592147c3906b176e4f88caa8f4b31be
InomataKensuke <inomata.kensuke@isopra.co.jp>
2019-05-31 10:48:09 +0900

    全画面の見た目と遷移の完成

--- src/main/java/jp/co/isopra/entryandexit/controller/HeloController.java
+++ src/main/java/jp/co/isopra/entryandexit/controller/HeloController.java
@@ -10,4 +10,49 @@ public class HeloController {
 	public String index() {
 		return "index";
 	}
+
+	@RequestMapping("/faceCapture")
+	public String faceCapture() {
+		return "faceCapture";
+	}
+
+	@RequestMapping("/recordDetail")
+	public String recordDetail() {
+		return "recordDetail";
+	}
+
+	@RequestMapping("record")
+	public String record() {
+		return "record";
+	}
+
+	@RequestMapping("/member")
+	public String member() {
+		return "member";
+	}
+
+	@RequestMapping("/memberFace")
+	public String memberFace() {
+		return "memberFace";
+	}
+
+	@RequestMapping("/memberEdit")
+	public String memberEdit() {
+		return "memberEdit";
+	}
+
+	@RequestMapping("/location")
+	public String location() {
+		return "location";
+	}
+
+	@RequestMapping("/locationNew")
+	public String locationNew() {
+		return "locationEdit";
+	}
+
+	@RequestMapping("/locationEdit")
+	public String locationEdit() {
+		return "locationEdit";
+	}
 }
