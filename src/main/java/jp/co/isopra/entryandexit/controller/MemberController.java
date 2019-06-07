a92ac60c72d151a4beff88e2175259c04d8fc56d
InomataKensuke <inomata.kensuke@isopra.co.jp>
2019-06-07 11:41:25 +0900

    メンバー管理作成しました。

--- src/main/java/jp/co/isopra/entryandexit/controller/MemberController.java
+++ src/main/java/jp/co/isopra/entryandexit/controller/MemberController.java
@@ -1,9 +1,16 @@
 package jp.co.isopra.entryandexit.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import jp.co.isopra.entryandexit.entity.Member;
@@ -18,40 +25,100 @@ public class MemberController {
 	@RequestMapping("/member")
 	public ModelAndView member(ModelAndView mav) {
 		mav.setViewName("member");
+		Iterable<Member> list = repository.findAllOrderById();
+		mav.addObject("list", list);
+		return mav;
+	}
+
+	@RequestMapping(value="/memberNew/{hidden}", method=RequestMethod.GET)
+	public ModelAndView memberNew(
+			@ModelAttribute("formModel") Member member,
+			@PathVariable int hidden ,
+			ModelAndView mav) {
+		mav.setViewName("memberEdit");
+		mav.addObject("formModel", member);
+		mav.addObject("hidden", hidden);
+		mav.addObject("check", hidden == 1);
+		return mav;
+	}
+
+	@RequestMapping(value="/memberNew/{hidden}", method=RequestMethod.POST)
+	public ModelAndView memberCreate(
+			@ModelAttribute("formModel")@Validated Member member,
+			@PathVariable int hidden,
+			BindingResult result,
+			ModelAndView mav) {
+		ModelAndView res = null;
+		if(!result.hasErrors()) {
+			repository.saveAndFlush(member);
+			res = new ModelAndView("redirect:/memberFace/" + hidden);
+		}else {
+			mav.setViewName("memberEdit");
+			mav.addObject("hidden", hidden);
+			mav.addObject("check", hidden == 1);
+			res = mav;
+		}
+		return res;
+	}
+
+	@RequestMapping(value="/memberEdit/{hidden}", method=RequestMethod.GET)
+	public ModelAndView memberEdit(
+			@ModelAttribute("formModel") Member member,
+			@RequestParam(value="member") int id,
+			@PathVariable int hidden,
+			ModelAndView mav) {
+		mav.setViewName("memberEdit");
+		mav.addObject("hidden", hidden);
+		mav.addObject("check", hidden == 1);
+		Optional<Member> data = repository.findById(id);
+		mav.addObject("formModel", data.get());
+		return mav;
+	}
+
+	@RequestMapping(value="/memberEdit/{hidden}", method=RequestMethod.POST)
+	public ModelAndView memberUpdate(
+			@ModelAttribute("formModel")@Validated Member member,
+			@PathVariable int hidden,
+			BindingResult result,
+			ModelAndView mav) {
+		ModelAndView res = null;
+		if(!result.hasErrors()) {
+			repository.saveAndFlush(member);
+			res = new ModelAndView("redirect:/member");
+		}else {
+			mav.setViewName("memberEdit");
+			mav.addObject("hidden", hidden);
+			mav.addObject("check", hidden == 1);
+			res = mav;
+		}
+		return res;
+	}
+
+	@RequestMapping("/memberFace/{hidden}")
+	public ModelAndView memberFace(
+			@ModelAttribute("formModel")Member member,
+			@PathVariable int hidden,
+			ModelAndView mav) {
+		mav.setViewName("memberFace");
+		mav.addObject("hidden", hidden);
+		mav.addObject("check", hidden == 1);
 		Iterable<Member> list = repository.findAll();
 		mav.addObject("list", list);
 		return mav;
 	}
 
-	@RequestMapping("/memberFace/{hidden}")
-	public ModelAndView memberFace(@PathVariable int hidden, ModelAndView mav) {
+	@RequestMapping("/memberRegistFace/{hidden}")
+	public ModelAndView memberRegistFace(
+			@ModelAttribute("formModel")Member member,
+			@RequestParam(value="member_id")int id,
+			@PathVariable int hidden,
+			ModelAndView mav) {
 		mav.setViewName("memberFace");
 		mav.addObject("hidden", hidden);
 		mav.addObject("check", hidden == 1);
+		Optional<Member> data = repository.findById(id);
+		mav.addObject("formModel", data.get());
 		return mav;
 	}
 
-	@RequestMapping("/memberUpdate/{hidden}")
-	public ModelAndView memberUpdate(@PathVariable int hidden, ModelAndView mav) {
-		mav.setViewName("memberEdit");
-		mav.addObject("hidden", hidden);
-		mav.addObject("check", hidden == 2);
-		return mav;
-	}
-
-	@RequestMapping("/memberEdit/{hidden}")
-	public ModelAndView memberEdit(@PathVariable int hidden ,ModelAndView mav) {
-		mav.setViewName("memberEdit");
-		mav.addObject("hidden", hidden);
-		mav.addObject("check", hidden == 1);
-		return mav;
-	}
-
-//	@RequestMapping("/memberEdit/{hidden}")
-//	public ModelAndView memberEdit(@PathVariable int hidden, ModelAndView mav) {
-//		mav.setViewName("memberEdit");
-//		mav.addObject("hidden", hidden);
-//		mav.addObject("check", hidden == 1);
-//		return mav;
-//	}
 }
