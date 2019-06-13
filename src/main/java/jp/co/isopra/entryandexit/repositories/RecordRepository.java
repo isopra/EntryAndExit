3903c15801656bc88c8f1998546177af95df466a
HatakeyamaTomohiro <hatakeyama.tomohiro@isopra.co.jp>
2019-06-13 16:29:48 +0900

    画面3の機能追加

--- src/main/java/jp/co/isopra/entryandexit/repositories/RecordRepository.java
+++ src/main/java/jp/co/isopra/entryandexit/repositories/RecordRepository.java
@@ -1,12 +1,18 @@
 package jp.co.isopra.entryandexit.repositories;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import jp.co.isopra.entryandexit.entity.Record;
 import jp.co.isopra.entryandexit.entity.RecordPK;
 
 @Repository
-public interface RecordRepository extends JpaRepository<Record, RecordPK> {
+public interface RecordRepository extends JpaRepository<Record,RecordPK> {
+
+	//public Optional<Record> findByDate(Date record_date);
+	@Query("SELECT r FROM Record r ORDER BY r.record_date")
+	Iterable<Record> findAllOrderByDate();
 
 }
