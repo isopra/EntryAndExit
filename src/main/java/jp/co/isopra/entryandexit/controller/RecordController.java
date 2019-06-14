15f0e1878ecb8c0167f02eb25a6295be174484cc
otashiro hiroki <otashiro.hiroki@isopra.co.jp>
2019-06-14 15:09:45 +0900

    recordの名前と拠点以外作成

--- src/main/java/jp/co/isopra/entryandexit/controller/RecordController.java
+++ src/main/java/jp/co/isopra/entryandexit/controller/RecordController.java
@@ -1,13 +1,49 @@
 package jp.co.isopra.entryandexit.controller;
 
+
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import jp.co.isopra.entryandexit.entity.Member;
+import jp.co.isopra.entryandexit.entity.Record;
+import jp.co.isopra.entryandexit.repositories.MemberRepository;
+import jp.co.isopra.entryandexit.repositories.RecordRepository;
 
 @Controller
 public class RecordController {
 
-	@RequestMapping("record")
-	public String record() {
-		return "record";
+	@Autowired
+	RecordRepository recordrepository;
+
+	@Autowired
+	MemberRepository memberrepository;
+
+
+	@RequestMapping("/record")
+	public ModelAndView record(ModelAndView mav,
+			@ModelAttribute Record record,
+			BindingResult result1,
+			@ModelAttribute Member member,
+			BindingResult result2) {
+		mav.setViewName("record");
+		Iterable<Record> recordT = recordrepository.findAll();
+		Iterable<Member> memberT = memberrepository.findAll();
+		mav.addObject("recordT",recordT);
+		mav.addObject("memberT",memberT);
+		if(result1.hasErrors() || result2.hasErrors()) {
+			for(ObjectError error:result1.getAllErrors()) {
+				System.out.println(error.getDefaultMessage());
+			}
+			for(ObjectError error:result2.getAllErrors()) {
+				System.out.println(error.getDefaultMessage());
+			}
+		}
+		return mav;
 	}
 }
---
+++ src/main/java/jp/co/isopra/entryandexit/entity/Record.java
@@ -0,0 +1,92 @@
+package jp.co.isopra.entryandexit.entity;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+	@Entity
+	@Table(name="record")
+	@IdClass(RecordPK.class)
+	public class Record {
+
+		@Id
+		@Column
+		private Date record_date;
+
+		@Id
+		@Column
+		private int location_id;
+
+		@Column
+		private int entry_member_id;
+
+		@Column
+		private Timestamp entry_time;
+
+		@Column
+		private int exit_member_id;
+
+		@Column
+		private Timestamp exit_time;
+
+		@Column
+		@NotNull
+		private Timestamp created_time;
+
+
+
+		public Date getRecord_date() {
+			return record_date;
+		}
+		public void setRecord_date(Date record_date) {
+			this.record_date = record_date;
+		}
+
+		public int getLocation_id() {
+			return location_id;
+		}
+		public void setLocation_id(int location_id) {
+			this.location_id = location_id;
+		}
+
+		public int getEntry_member_id() {
+			return entry_member_id;
+		}
+		public void setEntry_member_id(int entry_member_id) {
+			this.entry_member_id = entry_member_id ;
+		}
+
+		public Timestamp getEntry_time() {
+			return entry_time;
+		}
+		public void setEntry_time(Timestamp entry_time) {
+			this.entry_time = entry_time;
+		}
+
+		public int getExit_member_id() {
+			return exit_member_id;
+		}
+		public void setExit_member_id(int exit_member_id) {
+			this.exit_member_id = exit_member_id;
+		}
+
+		public Timestamp getExit_time() {
+			return exit_time;
+		}
+		public void setExit_time(Timestamp exit_time) {
+			this.exit_time = exit_time;
+		}
+
+		public Timestamp getCreated_time() {
+			return created_time;
+		}
+		public void setCreated_time(Timestamp created_time) {
+			this.created_time = created_time;
+		}
+	}
